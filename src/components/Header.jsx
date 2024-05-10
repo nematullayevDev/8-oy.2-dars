9b22fc790954f0f39803228bbaf2c1c345f5983b
nematullayevDev <webdargon@gmail.com>
2024-05-10 16:01:39 +0500

    8-OY.2-DARS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,23 @@ export function Header() {
     <div className="bg-white shadow-lg shadow-balck500/40 flex items-center  justify-between">
       <img className="pl-[24px] py-[20px]" src="Logo.svg" alt="" />
 
-      <ul className="navbar flex gap-[36px] ">
-        <li className="text-[#92929d] font-bold cursor-pointer">Asosiy</li>
-        <li className="text-[#92929d] font-bold cursor-pointer">
+      <ul className="navbar flex gap-[36px]  items-center">
+        <li className="text-[#92929d] font-bold cursor-pointer hover:text-[#0062ff] transition duration-150">
+          Asosiy
+        </li>
+        <li className="text-[#92929d] font-bold cursor-pointer hover:text-[#0062ff] transition duration-150">
           Mijozlar bazasi
         </li>
-        <li className="text-[#92929d] font-bold cursor-pointer">
-          Tracking loyihalari
+        <li className="h-[70px]  flex flex-col items-center  pt-[20px]">
+          <li className=" font-bold cursor-pointer text-[#0062ff]">
+            Tracking loyihalari
+          </li>
+          <div className="w-[105px] h-[3px] bg-[#0062ff] mt-6"></div>
         </li>
-        <li className="text-[#92929d] font-bold cursor-pointer">
+        <li className="text-[#92929d] font-bold cursor-pointer hover:text-[#0062ff] transition duration-150">
           Sharh olish loyihalari
         </li>
-        <li className="text-[#92929d] font-bold cursor-pointer">
+        <li className="text-[#92929d] font-bold cursor-pointer hover:text-[#0062ff] transition duration-150">
           Redirect loyihalari
         </li>
       </ul>
